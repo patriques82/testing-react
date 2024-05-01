ed87f83fbed173ea47cb1d7282120dbce5bbbe8e
Patrik Nygren <patriknygren82@gmail.com>
2024-05-01 14:28:00 +0200

    fixed css in tests

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,14 @@
+import "../index.css";
+
 const Note = ({ note, toggleImportance }) => {
-  const label = note.important
-    ? 'make not important' : 'make important'
+  const label = note.important ? "make not important" : "make important";
 
   return (
-    <li className='note'>
+    <li className="note">
       {note.content}
       <button onClick={toggleImportance}>{label}</button>
     </li>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
